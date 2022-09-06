b34db1b4371b013a3e8348a4e1a5d42d73837f9b
Pietro Fiori <78088814+pietrofiori@users.noreply.github.com>
2022-09-06 13:53:46 -0300

    Atualizações Modal

--- restaurante/restaurante/restaurante/httpfiles/wecom-restauranteadmin.js
+++ restaurante/restaurante/restaurante/httpfiles/wecom-restauranteadmin.js
@@ -38,6 +38,31 @@ Wecom.restauranteAdmin = Wecom.restauranteAdmin || function (start, args) {
         if (obj.api == "admin" && obj.mt == "AdminMessageResult") {
         }
     }
+    // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 }
 
 Wecom.restauranteAdmin.prototype = innovaphone.ui1.nodePrototype;
