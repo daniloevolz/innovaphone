010b3d2edbe9d609dca0b572214006e234857805
Danilo <46790719+daniloevolz@users.noreply.github.com>
2022-09-18 23:32:46 -0300

    Alterações esquecidas Danilo
    Talvez estrague o commit do Restaurante do Pietro

---
+++ wecall/wecall/x86_64-debug/obj/wecall/wecom-wecalladmin.js
@@ -0,0 +1,64 @@
+
+/// <reference path="../../web1/lib1/innovaphone.lib1.js" />
+/// <reference path="../../web1/appwebsocket/innovaphone.appwebsocket.Connection.js" />
+/// <reference path="../../web1/ui1.lib/innovaphone.ui1.lib.js" />
+
+var Wecom = Wecom || {};
+Wecom.wecallAdmin = Wecom.wecallAdmin || function (start, args) {
+    this.createNode("body");
+    var that = this;
+
+    var colorSchemes = {
+        dark: {
+            "--bg": "#191919",
+            "--button": "#303030",
+            "--text-standard": "#f2f5f6",
+        },
+        light: {
+            "--bg": "white",
+            "--button": "#e0e0e0",
+            "--text-standard": "#4a4a49",
+        }
+    };
+    var schemes = new innovaphone.ui1.CssVariables(colorSchemes, start.scheme);
+    start.onschemechanged.attach(function () { schemes.activate(start.scheme) });
+
+    var texts = new innovaphone.lib1.Languages(Wecom.wecallTexts, start.lang);
+
+    var app = new innovaphone.appwebsocket.Connection(start.url, start.name);
+    app.checkBuild = true;
+    app.onconnected = app_connected;
+    app.onmessage = app_message;
+
+    var elSalvarCloseModal = document.getElementById("salvarClose");
+    elSalvarCloseModal.addEventListener("click", function () { insertUrl() }, false);
+
+
+    function app_connected(domain, user, dn, appdomain) {
+        app.send({ api: "admin", mt: "AdminMessage" });
+    }
+
+    function app_message(obj) {
+        if (obj.api == "admin" && obj.mt == "AdminMessageResult") {
+            if (obj.src == "") {
+                document.getElementById('section1').style.display = 'block';
+                document.getElementById('section2').style.display = 'none';
+
+            } else {
+                document.getElementById('section2').style.display = 'block';
+                document.getElementById('section1').style.display = 'none';
+                document.getElementById("iframebody").setAttribute("src", obj.src);
+            }
+
+        }
+    }
+    function insertUrl() {
+        var urlPortal = document.getElementById("urlPortal").value;
+        if (urlPortal.length > 1) {
+            app.send({ api: "admin", mt: "AddMessage", url: String(urlPortal) });
+        }
+
+    }
+}
+
+Wecom.wecallAdmin.prototype = innovaphone.ui1.nodePrototype;
