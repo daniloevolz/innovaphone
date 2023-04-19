99fb096c999d6318e72c30e74637bc959c8b525c
Pietro Fiori <78088814+pietrofiori@users.noreply.github.com>
2023-04-19 17:20:20 -0300

    update

--- dwcscheduler/dwcscheduler/x86_64-debug/obj/dwcscheduler/wecom-dwcscheduler.js
+++ dwcscheduler/dwcscheduler/x86_64-debug/obj/dwcscheduler/wecom-dwcscheduler.js
@@ -69,8 +69,10 @@ Wecom.dwcscheduler = Wecom.dwcscheduler || function (start, args) {
     function app_message(obj) {
         if (obj.api == "user" && obj.mt == "UserMessageResult") {
             console.log(obj.result);
+            console.log("AAAAAAAAAAAAAAAAAAWWWWWWWWWWWWWWWWWWWWWWWWAAAAAA");
             list_configs = JSON.parse(obj.result);
             makeDivGeral(_colDireita);
+            
         }
         if (obj.api == "user" && obj.mt == "SelectAvailabilityMessageSuccess") {
             console.log(obj.result);
