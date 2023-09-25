99e1bf9ede3b23dd120fc449ce52caa8b0fb7aa8
Pietro Fiori <78088814+pietrofiori@users.noreply.github.com>
2023-09-25 14:09:07 -0300

    upda

--- coolwork/coolwork/coolwork/httpfiles/wecom-coolwork.js
+++ coolwork/coolwork/coolwork/httpfiles/wecom-coolwork.js
@@ -34,8 +34,6 @@ Wecom.coolwork = Wecom.coolwork || function (start, args) {
     app.onconnected = app_connected;
     app.onmessage = app_message;
 
-    
-    
     function app_connected(domain, user, dn, appdomain) {
         var devicesApi = start.consumeApi("com.innovaphone.devices");
         devicesApi.onmessage.attach(devicesApi_onmessage); // onmessage is called for responses from the API
@@ -53,11 +51,6 @@ Wecom.coolwork = Wecom.coolwork || function (start, args) {
         }
         
     }
-
-    function populateGridPhones(obj) {
-        console.log("populateGridPhones:GetPhonesResult " + JSON.stringify(obj));
-    }
-
     function app_message(obj) {
         if (obj.api === "user" && obj.mt === "PhoneListResult") {
             // placeholder for JsonApi handling
@@ -68,12 +61,14 @@ Wecom.coolwork = Wecom.coolwork || function (start, args) {
 
     }
 
-    var divmain = that.add(new innovaphone.ui1.Div("position:absolute;width:100%;height:100%;text-align:center;display:flex;justify-content:center;align-items:center",null,null).setAttribute("id","mainDiv"))
-    divmain.add(new innovaphone.ui1.Node("span", "", "MAC do Telefone:", ""));
+    var colEsquerda = that.add(new innovaphone.ui1.Div(null,null,"colEsquerda"));
+    colEsquerda.add(new innovaphone.ui1.Node("scroll-container",null,null,"scroll-container").setAttribute("id","scroll-phones"));
 
+    var colDireita = that.add(new innovaphone.ui1.Div(null,null,"colDireita"));
+    var divmain = colDireita.add(new innovaphone.ui1.Div("position:absolute;width:100%;height:100%;text-align:center;display:flex;justify-content:center;align-items:center",null,null).setAttribute("id","mainDiv"));
+    divmain.add(new innovaphone.ui1.Node("span", "", "MAC do Telefone:", ""));
     var inputHW = divmain.add(new innovaphone.ui1.Node("input", "", "", ""));
     inputHW.setAttribute("id", "hwinput").setAttribute("type", "text");
-
     var loginButton = divmain.add(new innovaphone.ui1.Div(null, null, "button")
         .addText("Login")
         .addEvent("click", function () { app.send({ api: "user", mt: "LoginPhone", hw: inputHW.value }); }, loginButton));
@@ -83,21 +78,19 @@ Wecom.coolwork = Wecom.coolwork || function (start, args) {
 
     function makePhoneButtons(obj){
 
-        
-        // console.log("domain" + dnz)
-        // console.log( "foto usuario" + UsersPictures)
         obj.forEach(function (phone) {
             var userPicture = avatar.url(phone.sip ,80)
-            console.log("SIP DO CARA" + phone.sip)
+            // console.log("SIP DO CARA" + phone.sip)
             var phoneHTML = `
             <div class="StatusPhone${phone.online} phoneButtons" id="${phone.hwId}">
-            <img class="imgProfile" src ="${userPicture}">
-            <div>${phone.cn}</div>
-            <span style = "font-weight:bold" >${phone.product}</span>
+            <div class="user-info">
+                <img class="imgProfile" src="${userPicture}">
+                <div class="user-name">${phone.cn}</div>
             </div>
+            <div class="product-name">${phone.product}</div>
+             </div>
             `;
-            console.log("work");
-            document.getElementById("mainDiv").innerHTML += phoneHTML;
+            document.getElementById("scroll-phones").innerHTML += phoneHTML;
         });
     }
    
