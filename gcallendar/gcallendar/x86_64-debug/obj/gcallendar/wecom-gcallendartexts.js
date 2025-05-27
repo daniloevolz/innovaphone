97ed89653b4a3bd449abbe126bc4dc9bbc76c1c0
Erick Niary Cardoso <135270097+erickcrds@users.noreply.github.com>
2025-05-27 15:30:22 -0300

    Revert "nomenclaturas"
    This reverts commit f40c12e428cc8be1825f8be70f72d4c462df93ff.

---
+++ gcallendar/gcallendar/x86_64-debug/obj/gcallendar/wecom-gcallendartexts.js
@@ -0,0 +1,56 @@
+﻿
+var Wecom = Wecom || {};
+Wecom.gcalendarTexts = {
+    pt: {
+        iptClientId: 'Google Client ID',
+        labelClientId: 'Client ID Google',
+        btnUpdate: 'Atualizar',
+        labelOrigins: 'URL de Redirecionamento',
+        iptOrigins: 'URL',
+        labelClientSecret: 'Client Secret',
+        iptClientSecret: 'Client Secret',
+        updated: 'Atualizado!',
+        btnConnect: 'Conectar',
+        btnDisconnect: 'Desconectar',
+        labelUri: 'Você vai precisar da URL abaixo para autorizar no Portal OAuth do Google'
+    },
+    en: {
+        iptClientId: 'Google Client ID',
+        labelClientId: 'Google Client ID',
+        btnUpdate: 'Update',
+        labelOrigins: 'Redirect URL',
+        iptOrigins: 'URL',
+        labelClientSecret: 'Client Secret',
+        iptClientSecret: 'Client Secret',
+        updated: 'Updated!',
+        btnConnect: 'Connect',
+        btnDisconnect: 'Disconnect',
+        labelUri: 'You will need the URL below to authorize on the Google OAuth Portal'
+    },
+    de: {
+        iptClientId: 'Google Client-ID',
+        labelClientId: 'Google Client-ID',
+        btnUpdate: 'Aktualisieren',
+        labelOrigins: 'Weiterleitungs-URL',
+        iptOrigins: 'URL',
+        labelClientSecret: 'Client-Geheimnis',
+        iptClientSecret: 'Client-Geheimnis',
+        updated: 'Aktualisiert!',
+        btnConnect: 'Verbinden',
+        btnDisconnect: 'Trennen',
+        labelUri: 'Sie benötigen die folgende URL, um sich im Google OAuth-Portal zu autorisieren.'
+    },
+    es: {
+        iptClientId: 'Google Cliente ID',
+        labelClientId: 'ID de Cliente de Google',
+        btnUpdate: 'Actualizar',
+        labelOrigins: 'URL de Redirecciõn',
+        iptOrigins: 'URL',
+        labelClientSecret: 'Secreto de Cliente',
+        iptClientSecret: 'Secreto de Cliente',
+        updated: 'Actualizado!',
+        btnConnect: 'Conectar',
+        btnDisconnect: 'Desconectar',
+        labelUri: 'Necesitará la siguiente URL para realizar la autorizaciõn en el Portal OAuth de Google'
+    }
+}
---
+++ gcallendar/gcallendar/x86_64/obj/gcallendar/wecom-gcallendar.js
@@ -0,0 +1,107 @@
+
+/// <reference path="../../web1/lib1/innovaphone.lib1.js" />
+/// <reference path="../../web1/appwebsocket/innovaphone.appwebsocket.Connection.js" />
+/// <reference path="../../web1/ui1.lib/innovaphone.ui1.lib.js" />
+
+var Wecom = Wecom || {};
+Wecom.gcalendar = Wecom.gcalendar || function (start, args) {
+    this.createNode("body");
+    var that = this;
+    var clientId;
+    var redirectUri
+    var token;
+    var guid;
+
+    var colorSchemes = {
+        dark: {
+            "--bg": "#191919",
+            "--button": "#303030",
+            "--text-standard": "#f2f5f6",
+        },
+        light: {
+            "--bg": "white",
+            "--button": "#e0e0e0",
+            "--text-standard": "#4a4a49",
+        }
+    };
+    var schemes = new innovaphone.ui1.CssVariables(colorSchemes, start.scheme);
+    start.onschemechanged.attach(function () { schemes.activate(start.scheme) });
+
+    var texts = new innovaphone.lib1.Languages(Wecom.gcalendarTexts, start.lang);
+    start.onlangchanged.attach(function () { texts.activate(start.lang) });
+
+    var app = new innovaphone.appwebsocket.Connection(start.url, start.name);
+    app.checkBuild = true;
+    app.onconnected = app_connected;
+    app.onmessage = app_message;
+
+    function app_connected(domain, user, dn, appdomain) {
+        app.send({ api: "user", mt: "UserMessage" });
+    }
+
+    function app_message(obj) {
+        if (obj.api == "user" && obj.mt == "UserMessageResult") {
+            clientId = obj.client_id;
+            redirectUri = obj.javascript_origins;
+            guid = obj.guid;
+            token = JSON.parse(obj.token);
+            createBody(that);
+        }
+        if (obj.api == "user" && obj.mt == "UserDisconnectResult") {
+            token = [];
+            createBody(that);
+            createToast(texts.text("updated"))
+        }
+    }
+    function createBody(t) {
+        t.clear();
+        if (token.length != 0) {
+            t.add(new innovaphone.ui1.Div("position:absolute; left:40%; width:20%; top:45%; font-size:15px; text-align:center", null, "button")).addTranslation(texts, "btnDisconnect").addEvent("click", function () {
+                app.send({ api: "user", mt: "UserDisconnect" });
+            });
+        } else {
+            t.add(new innovaphone.ui1.Div("position:absolute; left:40%; width:20%; top:45%; font-size:15px; text-align:center", null, "button")).addTranslation(texts, "btnConnect").addEvent("click", function () {
+                openOAuthPopup();
+            });
+        }
+
+    }
+    // Fun��o para abrir o popup do OAuth do Google
+    function openOAuthPopup() {
+        var scope = 'https://www.googleapis.com/auth/calendar.readonly';
+        var responseType = 'code';
+        var authUrl = 'https://accounts.google.com/o/oauth2/v2/auth?' +
+            'scope=' + encodeURIComponent(scope) + '&' +
+            'response_type=' + encodeURIComponent(responseType) + '&' +
+            'redirect_uri=' + encodeURIComponent(redirectUri) + '&' +
+            'client_id=' + encodeURIComponent(clientId) + '&' +
+            'state=' + encodeURIComponent(guid) + '&' +
+            'access_type=' + encodeURIComponent('offline') + '&' +
+            'prompt=' + encodeURIComponent('consent');
+        // Abrir o popup (tamanho e configura��o do popup)
+        var width = 600;
+        var height = 600;
+        var left = (screen.width / 2) - (width / 2);
+        var top = (screen.height / 2) - (height / 2);
+        window.open(authUrl, 'OAuthPopup', 'width=' + width + ',height=' + height + ',top=' + top + ',left=' + left);
+    }
+    // Fun��o para criar um toast de notifica��o
+    function createToast(message) {
+        var toast = that.add(new innovaphone.ui1.Div(null, message, "toast show"));
+        //var toast = document.createElement("div");
+        toast.setAttribute("id", "toast")
+
+        // Definir o texto do toast
+        //toast.innerHTML = message;
+
+        // Adicionar a classe 'show' para mostrar o toast
+        //toast.className = "toast show";
+
+        // Remover o toast ap�s 15 segundos
+        setTimeout(function () {
+            document.getElementById('toast').remove();
+        }, 5000); // 15 segundos
+    }
+}
+
+Wecom.gcalendar.prototype = innovaphone.ui1.nodePrototype;
